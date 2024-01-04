16295ab515ccd6d132050cc0342c6a81dbdb4ba1
Asher <ashersam116@gmail.com>
2024-01-03 15:08:26 -0900

    done with merge sort

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,21 +1,14 @@
 import React from "react";
 import "./content.scss";
+import SortingVisualizer from "../SortingVisualizer/SortingVisualizer";
 
 function Content() {
   return (
     <div className="content-container">
       <div className="content-card">
-        <div className="content-card-navbar">
-          <div className="content-card-navbar-button">Generate new Array</div>
-          <div className="content-card-navbar-button">
-            <div className="button-range">
-              <p>Change array size</p>
-              <input type="range" />
-            </div>
-          </div>
-          <div className="content-card-navbar-button">Merge Sort</div>
+        <div className="content-card-main">
+          <SortingVisualizer></SortingVisualizer>
         </div>
-        <div className="content-card-main"></div>
       </div>
     </div>
   );
